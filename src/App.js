a5f1f72184d09b97e1f854b0eb63ed1eb45680ff
Nathan Tutchton <ntutchton@gmail.com>
2018-09-20 22:13:36 -0600

    updating package.json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,12 @@ class App extends Component {
         <p className="App-intro">
           Create React App makes this project way easier... still working on getting my dependencies through the pipeline.
         </p>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <Button variant="outlined" color="primary">
+          Primary
+        </Button>
       </div>
     );
   }
