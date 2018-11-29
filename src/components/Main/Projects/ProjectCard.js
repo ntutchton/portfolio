2af52058bf2da7cbea947cb27305fa6a8be7dd51
Nathan Tutchton <ntutchton@gmail.com>
2018-11-29 14:38:28 -0700

    cleanup

--- src/components/Main/Projects/ProjectCard.js
+++ src/components/Main/Projects/ProjectCard.js
@@ -1,22 +1,97 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles';
-
+import Typography from '@material-ui/core/Typography';
+import SvgLogo from '../../Logo'
 
 const styles = theme => ({
   root: {
-    flex: '1',
-    height: '10em',
-    background: '#888',
+    maxWidth: '26.6vw',
+    padding: '1vw',
+    [theme.breakpoints.down('lg')]: {
+      maxWidth: '30vw',
+    },
+    [theme.breakpoints.down('md')]: {
+      maxWidth: '45vw'
+    },
   },
+  img: {
+    // flex: '1',
+    // height: '10em',
+    background: 'url(https://purr.objects-us-west-1.dream.io/i/rQIjIKH.jpg)',
+    backgroundImage: 'contain',
+    display: 'flex',
+    overflow: 'hidden',
+    // '&:after':{
+    //   content: '""',
+    //   display:'block',
+    //   paddingBottom: '100%',
+    // }
+  },
+  overlay: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    width: '100%',
+    backgroundColor: 'rgba(27,27,27,.7)'
+  },
+  overlaySvgWrapper: {
+    transform: 'scale(1.15) translateX(-4.5vw)',
+    opacity: '.2',
+  },
+  projectName: {
+    color: '#fafafa',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+
 })
 
-function ProjectCard(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <span>placholder</span>
-    </div>
-  );
+class ProjectCard extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      imgHeight: 0
+    }
+    this.cardRoot = React.createRef()
+  }
+
+  componentDidMount(){
+    this.recalculateImgHeight()
+    window.addEventListener('resize', this.recalculateImgHeight)
+  }
+
+  //set height to width of element after sudtracting horizontal padding from width (i.e. innerWidth or element)
+  recalculateImgHeight = () => {
+    this.setState({
+      imgHeight : this.cardRoot.current.clientWidth - (parseFloat(getComputedStyle(this.cardRoot.current).paddingLeft) + parseFloat(getComputedStyle(this.cardRoot.current).paddingRight))
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div ref={this.cardRoot} className={classes.root}>
+        <div className={classes.img} style={{height: `${this.state.imgHeight}px`}}>
+          <div className={classes.overlay}>
+            <div className={classes.overlaySvgWrapper}>
+              <SvgLogo type={'light'} size={this.state.imgHeight}></SvgLogo>
+            </div>
+          </div>
+        </div>
+        <div  >
+          <Typography variant="h4" className={classes.projectName}>
+            Placeholder
+          </Typography>
+        </div>
+      </div>
+    );
+  }
+}
+
+ProjectCard.propType = {
+  // height: PropTypes.number.isRequired,
 }
 
 export default withStyles(styles)(ProjectCard);
--- src/components/Main/Projects/index.js
+++ src/components/Main/Projects/index.js
@@ -7,26 +7,17 @@ const styles = theme => ({
   root: {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr 1fr',
-    // gridTemplateRows: '1fr 1fr 1fr 1fr',
-    gridColumnGap: '3em',
-    gridRowGap: '3em',
     margin: '1em 10%',
     [theme.breakpoints.down('lg')]: {
       margin: '1em 5%',
     },
     [theme.breakpoints.down('md')]: {
-      margin: '1em 10px',
       gridTemplateColumns: '1fr 1fr',
-      gridColumnGap: '5%',
     },
   }
 })
 
 class Projects extends React.Component{
-
-  state = {
-
-  }
 
   render(){
     const { classes } = this.props
